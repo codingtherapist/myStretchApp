63023d831a81d05b50a035132ad3d749e8c18feb
Africa Mincey <africamincey@gmail.com>
2022-10-11 16:29:45 -0400

    logout needs to be fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,8 +47,9 @@ exports.postLogin = (req, res, next) => {
 exports.logout = (req, res) => {
   req.logout(() => {
     console.log('User has logged out.')
+   res.redirect("/")
   })
-  req.session.destroy((err) => {
+  req.session.regenerate((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
     req.user = null;
